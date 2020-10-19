8710eb00472c06f34612f98b238665a7130b1cbd
kpitlawar <pitlawarkanchan.gmail.com>
2020-10-19 10:09:04 +0530

    Added Tab Layout

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -6,20 +6,53 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-      
-        title: Text('Profile',
+   
+     return DefaultTabController(
+       length: 3,
+            child: Scaffold(
+        appBar: AppBar(title: Text('Profile',
         style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.blue,
         elevation: 0.0,
-      ),
-      body: 
-      Center(child: Text('In Profile Screen ')),
-      
-    );
+        backgroundColor: Colors.blue[500],
+        bottom: TabBar(
+          unselectedLabelColor: Colors.grey[700],
+        labelColor: Colors.white,
+          tabs: [
+          Tab(child: Text('Edit'),
+          icon: Icon(Icons.edit, size: 15,),
+          
+          ),
+          Tab(child: Text('Delete'),
+          icon: Icon(Icons.delete, size: 15,),
+          ),
+          Tab(child: Text('New'),
+          icon: Icon(Icons.add_circle, size: 15,),
+
+          ),
+        ],),
+        ),
+        body: 
+        TabBarView(children: [
+          // Container(
+          //   child: Text('In Edit'),
+          // ),
+          //  Container(
+          //   child: Text('In Delete'),
+          // ),
+          //  Container(
+          //   child: Text('In New'),
+          // ),
+          Icon(Icons.edit, size: 30,),
+          Icon(Icons.delete, size: 30,),
+          Icon(Icons.add_circle, size: 30,)
+        ],)
+        // Center(child: Text('In Profile Screen ')),
+    ),
+     );
   }
 }
